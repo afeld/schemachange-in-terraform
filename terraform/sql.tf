6405264177e2506d47c0f1359f92363eb7ea0b84
Aidan Feldman <aidan.feldman@gmail.com>
2024-06-06 00:34:40 -0400

    refactor to do file listing separate from trimming

--- terraform/sql.tf
+++ terraform/sql.tf
@@ -4,12 +4,15 @@ locals {
   # use the basename of the file as the key (since "schemachange only pays attention to the filenames, not the paths"), the value as the full path
   # https://github.com/Snowflake-Labs/schemachange?tab=readme-ov-file#folder-structure
 
-  repeatable_files = { for filename in fileset(local.sql_root, "**/R__*.sql") : basename(filename) => "${local.sql_root}/${filename}" }
+  repeatable_files = fileset(local.sql_root, "**/R__*.sql")
+  repeatable_map   = { for filename in local.repeatable_files : basename(filename) => "${local.sql_root}/${filename}" }
 
   # doesn't strictly limit to digits, but close enough
-  versioned_files = { for filename in fileset(local.sql_root, "**/V*__*.sql") : basename(filename) => "${local.sql_root}/${filename}" }
+  versioned_files = fileset(local.sql_root, "**/V*__*.sql")
+  versioned_map   = { for filename in local.versioned_files : basename(filename) => "${local.sql_root}/${filename}" }
 
-  always_files = { for filename in fileset(local.sql_root, "**/A__*.sql") : basename(filename) => "${local.sql_root}/${filename}" }
+  always_files = fileset(local.sql_root, "**/A__*.sql")
+  always_map   = { for filename in local.always_files : basename(filename) => "${local.sql_root}/${filename}" }
 }
 
 # in theory, the snowflake_unsafe_execute resource would have more direct, but it was giving me 400 errors
@@ -17,7 +20,7 @@ locals {
 
 
 resource "null_resource" "versioned_sql" {
-  for_each = local.versioned_files
+  for_each = local.versioned_map
 
   provisioner "local-exec" {
     command = "snow sql -f ${each.value}"
@@ -25,7 +28,7 @@ resource "null_resource" "versioned_sql" {
 }
 
 resource "null_resource" "repeatable_sql" {
-  for_each = local.repeatable_files
+  for_each = local.repeatable_map
 
   triggers = {
     sql = file(each.value)
@@ -39,7 +42,7 @@ resource "null_resource" "repeatable_sql" {
 }
 
 resource "null_resource" "always_sql" {
-  for_each = local.always_files
+  for_each = local.always_map
 
   triggers = {
     sql = file(each.value)
