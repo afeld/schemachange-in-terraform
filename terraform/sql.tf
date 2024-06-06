ac3ef8971313e19f6a4ddf37a52f03d770f57336
Aidan Feldman <aidan.feldman@gmail.com>
2024-06-05 23:28:52 -0400

    load all repeatable scripts

--- terraform/sql.tf
+++ terraform/sql.tf
@@ -1,16 +1,18 @@
 locals {
-  path = "${path.module}/../sql/R__setup.sql"
-  sql  = file(local.path)
+  sql_root         = "${path.module}/../sql"
+  repeatable_files = fileset(local.sql_root, "**/R__*.sql")
 }
 
 # in theory, the snowflake_unsafe_execute resource would have more direct, but it was giving me 400 errors
 # https://registry.terraform.io/providers/Snowflake-Labs/snowflake/latest/docs/resources/unsafe_execute
-resource "null_resource" "sql" {
+resource "null_resource" "repeatable_sql" {
+  for_each = local.repeatable_files
+
   triggers = {
-    sql = local.sql
+    sql = file("${local.sql_root}/${each.key}")
   }
 
   provisioner "local-exec" {
-    command = "dotenv run -- snow sql --temporary-connection --authenticator Snowflake -f ${local.path}"
+    command = "dotenv run -- snow sql --temporary-connection --authenticator Snowflake -f ${local.sql_root}/${each.key}"
   }
 }
